a97ead6d7c0ba40687ba09f13d8a2f7e5a1f3b84
raiton685 <nemesis6850@gmail.com>
2026-03-03 22:32:28 +0300

    03.03

---
+++ src/Week1/Day1/Vizitka.java
@@ -0,0 +1,10 @@
+package Week1.Day1;
+
+class Vizitka {
+    public static void main(String[] args) {
+
+        System.out.println("Шестернев Кирилл Андреевич");
+        System.out.println("    8 903-105-15-85");
+    }
+}
+
---
+++ src/Week1/Day2/Hour.java
@@ -0,0 +1,14 @@
+package Week1.Day2;
+
+public class Hour {
+
+    public static void main(String[] args) {
+        int allMinutes = 890;
+
+        int hours = allMinutes / 60;
+        int minutes = allMinutes % 60;
+        System.out.println("Количество часов и минут: " + hours + ":" + minutes);
+
+    }
+}
+
